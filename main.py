30eb32d2d88c24749ccf232d8e115bce80710c33
rishabh416 <mittalr2008@gmail.com>
2024-06-04 18:01:11 +0400

    added picamera functionality

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import cv2
+from picamera2 import Picamera2
+
+cv2.startWindowThread()
+
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration(main={"format": 'RGB8888', "size": (640, 480)}))
+picam2.start()
 
 cap = cv2.VideoCapture(0)
 
 while True: 
-    ret, frame = cap.read()
+    frame = picam2.capture_array()
+
     cv2.imshow("Video Output", frame)
-    if cv2.waitKey(1) == ord('q'):
-        break
+    cv2.waitKey(1)
 
 cap.release()
 cv2.destroyAllWindows()
