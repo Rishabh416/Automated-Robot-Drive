0005e57eaad5c3bb186f1969574b89e132c12ffb
Rishabh416 <mittalr2008@gmail.com>
2024-06-06 16:52:24 +0400

    adding edge detection

--- main.py
+++ main.py
@@ -11,10 +11,16 @@ cap = cv2.VideoCapture(0)
 
 while True: 
     frame = picam2.capture_array()
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    blur = cv2.GaussianBlur(gray, (13,13), 0)
-    edges = cv2.Canny(blur, 85, 100)
-    cv2.imshow("Video Output", edges)
+    image = cv2.rotate(frame, cv2.ROTATE_180)
+    cropped = image[320:480,0:640]
+    gray = cv2.cvtColor(cropped, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (7,7), 0)
+    edges = cv2.Canny(blur, 50, 100)
+    lines = cv2.HoughLinesP(edges,1,0.01745,100,minLineLength=100,maxLineGap=10)
+    for line in lines:
+        x1,y1,x2,y2 = line[0]
+        cv2.line(blur,(x1,y1),(x2,y2),(0,255,0),2)
+    cv2.imshow("Video Output", blur)
     cv2.waitKey(1)
 
 cap.release()
