1f03ed5d8a39bb3c28812e7a833c381caf5c1171
rishabh416 <mittalr2008@gmail.com>
2024-07-12 12:44:24 +0400

    Created webui controller

--- main.py
+++ main.py
@@ -1,28 +1,62 @@
-import cv2
-from picamera2 import Picamera2
+from fastapi import FastAPI 
+from fastapi.responses import StreamingResponse 
+from fastapi.middleware.cors import CORSMiddleware 
+import uvicorn 
+from picamera2 import Picamera2  
+import cv2 
+import time 
 
-cv2.startWindowThread()
+app = FastAPI()
 
-picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
-picam2.start()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-while True: 
-    frame = picam2.capture_array()
-    image = cv2.rotate(frame, cv2.ROTATE_180)
-    cropped = image[320:480,0:640]
-    gray = cv2.cvtColor(cropped, cv2.COLOR_RGB2GRAY)
-    blur = cv2.GaussianBlur(gray, (13,13), 0)
-    equalizedImage = cv2.equalizeHist(blur)
-    edges = cv2.Canny(equalizedImage, 70, 100)
-    lines = cv2.HoughLinesP(edges,1,0.01745,100,minLineLength=20,maxLineGap=25)
-    if lines is not None:
-        for line in lines:
-            x1,y1,x2,y2 = line[0]
-            cv2.line(cropped,(x1,y1),(x2,y2),(0,0,255),2)
-    cv2.imshow("edges", edges)
-    cv2.imshow("Video Output", cropped)
-    cv2.waitKey(1)
+cam = Picamera2()
+config = cam.create_preview_configuration(main={"size": (640, 480)})
+cam.configure(config)  
+cam.start()  
 
-cap.release()
-cv2.destroyAllWindows()
+def generate_frames():
+    while True:  
+        frame = cam.capture_array()  
+        ret, buffer = cv2.imencode('.jpg', frame)  
+        frame = buffer.tobytes()  
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.get("/videoFeed")
+async def videoFeed():
+    return StreamingResponse(generate_frames(), media_type="multipart/x-mixed-replace; boundary=frame")
+
+@app.get("/takeImage")
+async def takeImage():
+    cam.capture_file(f"{time.time()}.jpg")
+    return("captured")
+
+@app.get("/forward")
+async def forward():
+    pass
+
+@app.get("/backward")
+async def forward():
+    pass
+
+@app.get("/right")
+async def forward():
+    pass
+
+@app.get("/left")
+async def forward():
+    pass
+
+@app.get("/stop")
+async def forward():
+    pass
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
